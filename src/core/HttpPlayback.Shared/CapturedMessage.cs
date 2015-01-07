4e627ff7ce6f5ee5a6313a592291dbff1e2b8eb9
Aaron Stannard <aaron@markedup.com>
2015-01-07 12:38:18 -0800

    created shared message library

---
+++ src/core/HttpPlayback.Shared/CapturedMessage.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Specialized;
+
+namespace HttpPlayback.Shared
+{
+    /// <summary>
+    /// Represents a captured HTTP request - this is the raw format that is used
+    /// to send HTTP requests we want to re-use over the wire to our capture server.
+    /// </summary>
+    public sealed class CapturedMessage
+    {
+        public CapturedMessage() : this(new NameValueCollection()) { }
+
+        public CapturedMessage(NameValueCollection httpHeaders)
+        {
+            HttpHeaders = httpHeaders;
+        }
+
+        /// <summary>
+        /// Represents the HTTP verb that was used in this request
+        /// </summary>
+        public string HttpVerb { get; set; }
+
+        /// <summary>
+        /// The full URI of the HTTP request, including host and port.
+        /// </summary>
+        public string AbsoluteUri { get; set; }
+
+        /// <summary>
+        /// The user agent for this request
+        /// </summary>
+        public string UserAgent { get; set; }
+
+        /// <summary>
+        /// Collection of all of the HTTP headers for this request
+        /// </summary>
+        public NameValueCollection HttpHeaders { get; private set; }
+
+        /// <summary>
+        /// The raw HTTP body of the request
+        /// </summary>
+        public string Body { get; set; }
+    }
+}
